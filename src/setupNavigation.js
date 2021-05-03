a0a421dd2ce363fc0d54fbfbc588cff5723a4856
Annemarie Wisbar <a.wisbar@gmail.com>
2021-05-03 22:12:36 +0200

    Update Navigation with fforEach functions

--- src/setupNavigation.js
+++ src/setupNavigation.js
@@ -1,72 +1,35 @@
+import getAllElements from './utils/getAllElements'
 import getElement from './utils/getElement'
 
 export default function setupNavigation() {
-  const homePage = getElement('.home-page')
-  const bookmarksPage = getElement('.bookmarks-page')
-  const createPage = getElement('.create-page')
-  const profilePage = getElement('.profile-page')
-
-  const buttonHome = getElement('.button-home')
-  const buttonBookmarks = getElement('.button-bookmarks')
-  const buttonCreate = getElement('.button-create')
-  const buttonProfile = getElement('.button-profile')
-
+  const pages = getAllElements('[data-page]')
+  const buttons = getAllElements('[data-nav]')
   const header = getElement('[data-header]')
 
-  buttonHome.addEventListener('click', navigateToHome)
-  buttonBookmarks.addEventListener('click', navigateToBookmarks)
-  buttonCreate.addEventListener('click', navigateToCreate)
-  buttonProfile.addEventListener('click', navigateToProfile)
+  buttons.forEach(button => {
+    const buttonName = button.dataset.nav
+    const headerText = button.dataset.headerText
 
-  function navigateToHome() {
-    header.innerText = 'Quiz App'
+    button.addEventListener('click', () => {
+      header.innerText = headerText
+      changePage(buttonName)
+      updateNavigation(buttonName)
+    })
+  })
 
-    changePage(homePage)
-    selectButton(buttonHome)
+  function changePage(buttonName) {
+    pages.forEach(page => {
+      const pageName = page.dataset.page
+      page.classList.toggle('hidden', pageName !== buttonName)
+    })
   }
 
-  function navigateToBookmarks() {
-    header.innerText = 'Bookmarks'
-
-    changePage(bookmarksPage)
-    selectButton(buttonBookmarks)
-  }
-
-  function navigateToCreate() {
-    header.innerText = 'New Question'
-
-    changePage(createPage)
-    selectButton(buttonCreate)
-  }
-
-  function navigateToProfile() {
-    header.innerText = 'Profile'
-
-    changePage(profilePage)
-    selectButton(buttonProfile)
-  }
-
-  function changePage(page) {
-    hideAllPages()
-    page.classList.remove('hidden')
-  }
-
-  function selectButton(button) {
-    deactivateAllButtons()
-    button.classList.add('active-nav-item')
-  }
-
-  function hideAllPages() {
-    homePage.classList.add('hidden')
-    bookmarksPage.classList.add('hidden')
-    createPage.classList.add('hidden')
-    profilePage.classList.add('hidden')
-  }
-
-  function deactivateAllButtons() {
-    buttonHome.classList.remove('active-nav-item')
-    buttonBookmarks.classList.remove('active-nav-item')
-    buttonCreate.classList.remove('active-nav-item')
-    buttonProfile.classList.remove('active-nav-item')
+  function updateNavigation(buttonName) {
+    buttons.forEach(button => {
+      button.classList.toggle(
+        'active-nav-item',
+        buttonName === button.dataset.nav
+      )
+    })
   }
 }
